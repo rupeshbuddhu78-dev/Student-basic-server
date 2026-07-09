3bd26992e641b776f835d39a7d0082cc886d28fb
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-07-09 07:10:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.get('/', (req, res) => {
 });
 
 console.log(process.platform);
-
+console.log(process.version);
+console.log(process.cwd());
+console.log(process.pid);
+console.log(process.env);
+console.log(process.env.USERNAME);
 // ==========================================
 // 1. MONGODB ONLINE DATABASE CONNECTION
 // ==========================================
